8404e81167af16ae3f9ebb2983ece9134a06a875
sree <umusunuri@gmail.com>
2016-07-05 18:48:04 -0700

    First commit

---
+++ functest.py
@@ -0,0 +1,6 @@
+def myfunction(x1, x2, x3):
+    y=x1+x2
+    z=y*x3
+    return z
+myresult = myfunction (2, 3, 4)
+print myresult
---
+++ prime.py
@@ -0,0 +1,13 @@
+# def prime_num(x):
+#     for i in range(2, x):
+#         if (x%i)==0:
+#             return False
+#     return True
+        
+#print prime_num(4)
+
+class Calculator(object):
+    def add(self, x, y):
+        return x+y
+
+        
